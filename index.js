a6b6c33a1d85005b68ec48ca74e4961b0fb77553
Christopher Thai <christopherthai.nyli@gmail.com>
2024-02-23 17:47:18 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+const findMatching = (listofDrivers, driver) => {
+  return listofDrivers.filter((element) => {
+    if (element.toLowerCase() === driver.toLowerCase()) {
+      return element
+    }
+  })
+}
+
+
+const fuzzyMatch = (listofDrivers, driver) => {
+  return listofDrivers.filter((element) => {
+    if (element.toLowerCase().substring(0, 2) === driver.toLowerCase()) {
+      return element
+    }
+    // return all drivers whose name begin with the provided letters
+  })
+}
+
+const matchName = (listofDrivers, driver) => {
+  return listofDrivers.filter((element) => {
+    if (element.name === driver) {
+      return element
+    }
+  })
+}
